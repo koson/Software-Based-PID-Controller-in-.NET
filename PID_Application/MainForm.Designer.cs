8a75373dfd54d5d30c3ac57a76d96c7534b288ee
Tyler Dula <37709933+Dulatr@users.noreply.github.com>
2018-04-03 23:18:46 -0400

    Garbage Collection
    Add explicit dispose of temperatureController

--- PID_Application/MainForm.Designer.cs
+++ PID_Application/MainForm.Designer.cs
@@ -16,6 +16,7 @@
             if (disposing && (components != null))
             {
                 components.Dispose();
+                temperatureController.Dispose();
             }
             base.Dispose(disposing);
         }
